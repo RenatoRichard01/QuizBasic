27df86b2e2a40a202f291872ca391dcde64113b8
Renato Richard <renatorichard2012@gmail.com>
2020-06-30 22:58:09 -0300

    Aprimorando executável

--- project_quest/lib/main.dart
+++ project_quest/lib/main.dart
@@ -6,15 +6,40 @@ main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
 
+  void responder(){
+    print('Pergunta respondida!');
+  }
+
 @override
   Widget build(BuildContext context){
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home:Scaffold(
         //arvore de componentes baseadas em composção;
         appBar: AppBar(
           title: Text('Perguntas')
         ),
-        body:Text('Olá flutter!'),
+        body:Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+                        RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+                        RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       ),
     );
     
